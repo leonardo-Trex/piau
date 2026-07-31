0c7c51ce6e5b969c08c84d7e7bf6a76ab50a9cd9
Leonardo <leonardosoares@unitins.br>
2026-07-31 15:18:01 -0300

    repository/: Fix @AplicationScoped missing

--- src/main/java/mercurio/repository/GroupProjectRepository.java
+++ src/main/java/mercurio/repository/GroupProjectRepository.java
@@ -1,7 +1,9 @@
 package mercurio.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import mercurio.model.GroupProject;
 
+@ApplicationScoped
 public class GroupProjectRepository implements PanacheRepository<GroupProject> {
 }
--- src/main/java/mercurio/repository/TopicRepository.java
+++ src/main/java/mercurio/repository/TopicRepository.java
@@ -1,8 +1,10 @@
 package mercurio.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 import mercurio.model.Topic;
 
+@ApplicationScoped
 public class TopicRepository implements PanacheRepository<Topic> {
 
 }
