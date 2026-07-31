221b596feeaf580914962c49f8803e2e26a707aa
Leonardo <leonardosoares@unitins.br>
2026-07-31 15:44:00 -0300

    import.sql/: Add inserts to group_project table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
+INSERT INTO group_projects(description, deadline, my_role, status)
+VALUES
+    ('Trabalho A1 - Semana Acadêmica', '2026-02-01', 'Pesquisar',0),
+    ('Trabalho A2 - Apresentação', '2026-03-01', 'Canvas',0);
+
 INSERT INTO exams(description, deadline, status)
 VALUES
     ('Prova de Cálculo I', '2026-02-01', 0),
