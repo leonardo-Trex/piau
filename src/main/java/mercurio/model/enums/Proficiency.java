f1b045c32683197cad6c1263845f54f56c9a01d8
Leonardo <leonardosoares@unitins.br>
2026-07-31 16:41:21 -0300

    enums/: Feat enum updated

--- src/main/java/mercurio/model/enums/Proficiency.java
+++ src/main/java/mercurio/model/enums/Proficiency.java
@@ -1,8 +1,35 @@
 package mercurio.model.enums;
 
-// TODO upgrade this guy
 public enum Proficiency {
-    KNOW_IT_WELL,
-    KNOW_NOTHING,
-    SO_SO
+    KNOW_IT_WELL(0L, "KNOW_IT_WELL"),
+    KNOW_NOTHING(1L, "KNOW_NOTHING"),
+    SO_SO(2L, "SO_SO");
+
+    private final Long id;
+    private final String description;
+
+    private Proficiency(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public static Proficiency of(Long id) {
+        if (id == null)
+            return null;
+
+        for (Proficiency p : values()) {
+            if(p.getId().equals(id))
+                return p;
+        }
+
+        return null; // TODO maybe a exception match here..
+    }
 }
--- src/main/java/mercurio/model/enums/Status.java
+++ src/main/java/mercurio/model/enums/Status.java
@@ -1,8 +1,35 @@
 package mercurio.model.enums;
 
-// TODO upgrade this guy
 public enum Status {
-    GUARANTEED,
-    NEED_GRADE,
-    FIFTY_FIFTY
+    GUARANTEED(0L, "GUARANTEED"),
+    NEED_GRADE(1L, "NEED_GRADE"),
+    FIFTY_FIFTY(2L, "FIFTY_FIFTY");
+
+    private final Long id;
+    private final String description;
+
+    private Status(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public static Status of(Long id) {
+        if (id == null)
+            return null;
+
+        for (Status s : values()) {
+            if(s.getId().equals(id))
+                return s;
+        }
+
+        return null; // TODO maybe a exception match here..
+    }
 }
