237ca699829bec9f08fdc5f6df8f34fffc2e3715
Leonardo <leonardosoares@unitins.br>
2026-07-31 15:17:47 -0300

    import.sql: DB feeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,9 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+INSERT INTO exams(description, deadline, status)
+VALUES
+    ('Prova de Cálculo I', '2026-02-01', 0),
+    ('Prova de Cálculo I A2', '2026-03-01', 0);
+
+INSERT INTO topics(description, proficiency, exam_id)
+VALUES
+    ('Derivadas', 0, 1),
+    ('Integrais', 0, 1);
